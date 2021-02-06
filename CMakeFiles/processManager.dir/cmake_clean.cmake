4c39d23bb188e0242ae1ecf511fb79a6fff93bd3
Aidan2803 <aid2803@ukr.net>
2021-02-06 05:32:56 -0800

    Compiled project with CMake

---
+++ CMakeFiles/processManager.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/processManager.dir/testTestTask_processManager.cpp.o"
+  "processManager"
+  "processManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/processManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
